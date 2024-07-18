0a81bcaab548f694a4db59bdb1c9c988fa4f2a6b
YutoNakamitsu <54886961+uutann@users.noreply.github.com>
2024-07-18 01:19:46 +0000

    fix home/about/help

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  root "hello#index"
+  get 'foo/bar'
+  get 'foo/baz'
+  get 'static_pages/home'
+  get 'static_pages/help'
+  get 'static_pages/about'
+  # root 'static_pages#home'
 end
